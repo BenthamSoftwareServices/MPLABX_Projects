9e884a9a6085700b3ba15c01365179476f2ed60b
Steven Bennett <benthamsoftwareservices@gmail.com>
2025-05-25 13:25:09 +0100

    Updates

---
+++ AtTiny1614/AtTiny1614_blink_led.X/mcc_generated_files/system/src/pins.c
@@ -0,0 +1,110 @@
+/**
+ * Generated Driver File
+ * 
+ * @file pins.c
+ * 
+ * @ingroup  pinsdriver
+ * 
+ * @brief This is generated driver implementation for pins. 
+ *        This file provides implementations for pin APIs for all pins selected in the GUI.
+ *
+ * @version Driver Version 1.1.0
+*/
+
+/*
+© [2024] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#include "../pins.h"
+
+static void (*LED_PIN_InterruptHandler)(void);
+
+void PIN_MANAGER_Initialize()
+{
+
+  /* OUT Registers Initialization */
+    PORTA.OUT = 0x0;
+    PORTB.OUT = 0x0;
+
+  /* DIR Registers Initialization */
+    PORTA.DIR = 0x8;
+    PORTB.DIR = 0x0;
+
+  /* PINxCTRL registers Initialization */
+    PORTA.PIN0CTRL = 0x0;
+    PORTA.PIN1CTRL = 0x0;
+    PORTA.PIN2CTRL = 0x0;
+    PORTA.PIN3CTRL = 0x0;
+    PORTA.PIN4CTRL = 0x0;
+    PORTA.PIN5CTRL = 0x0;
+    PORTA.PIN6CTRL = 0x0;
+    PORTA.PIN7CTRL = 0x0;
+    PORTB.PIN0CTRL = 0x0;
+    PORTB.PIN1CTRL = 0x0;
+    PORTB.PIN2CTRL = 0x0;
+    PORTB.PIN3CTRL = 0x0;
+    PORTB.PIN4CTRL = 0x0;
+    PORTB.PIN5CTRL = 0x0;
+    PORTB.PIN6CTRL = 0x0;
+    PORTB.PIN7CTRL = 0x0;
+
+  /* PORTMUX Initialization */
+    PORTMUX.CTRLA = 0x0;
+    PORTMUX.CTRLB = 0x0;
+    PORTMUX.CTRLC = 0x0;
+    PORTMUX.CTRLD = 0x0;
+
+  // register default ISC callback functions at runtime; use these methods to register a custom function
+    LED_PIN_SetInterruptHandler(LED_PIN_DefaultInterruptHandler);
+}
+
+/**
+  Allows selecting an interrupt handler for LED_PIN at application runtime
+*/
+void LED_PIN_SetInterruptHandler(void (* interruptHandler)(void)) 
+{
+    LED_PIN_InterruptHandler = interruptHandler;
+}
+
+void LED_PIN_DefaultInterruptHandler(void)
+{
+    // add your LED_PIN interrupt custom code
+    // or set custom function using LED_PIN_SetInterruptHandler()
+}
+ISR(PORTA_PORT_vect)
+{ 
+    // Call the interrupt handler for the callback registered at runtime
+    if(VPORTA.INTFLAGS & PORT_INT3_bm)
+    {
+       LED_PIN_InterruptHandler(); 
+    }
+    /* Clear interrupt flags */
+    VPORTA.INTFLAGS = 0xff;
+}
+
+ISR(PORTB_PORT_vect)
+{ 
+    /* Clear interrupt flags */
+    VPORTB.INTFLAGS = 0xff;
+}
+
+/**
+ End of File
+*/
