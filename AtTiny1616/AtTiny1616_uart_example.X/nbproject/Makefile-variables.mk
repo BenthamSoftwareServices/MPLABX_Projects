1044019c95d3a2d9e425e685bc7152f6abc81359
Steven Bennett <benthamsoftwareservices@gmail.com>
2025-05-25 13:27:38 +0100

    Updates

---
+++ AtTiny1616/AtTiny1616_uart_example.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=AtTiny1616_uart_example.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/AtTiny1616_uart_example.X.production.hex
